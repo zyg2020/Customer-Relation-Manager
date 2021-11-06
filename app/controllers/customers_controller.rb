b05eb9d0db07c67bf19046cbb21604254c7429bd
zyg2020 <zhuyange2018@gmail.com>
2021-11-06 13:49:32 -0500

    Add 3 actions and views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,14 +1,21 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: %i[ show edit update destroy ]
+  before_action :set_customer, only: %i[show edit update destroy]
 
   # GET /customers or /customers.json
   def index
     @customers = Customer.all
   end
 
-  # GET /customers/1 or /customers/1.json
-  def show
+  def alphabetized
+    @customers = Customer.all.order(:full_name)
   end
+
+  def missing_email
+    @customers = Customer.where(email_address: [nil, ""])
+  end
+
+  # GET /customers/1 or /customers/1.json
+  def show; end
 
   # GET /customers/new
   def new
@@ -16,8 +23,7 @@ class CustomersController < ApplicationController
   end
 
   # GET /customers/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /customers or /customers.json
   def create
@@ -57,13 +63,14 @@ class CustomersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      @customer = Customer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.require(:customer).permit(:full_name, :phone_number, :email_address, :notes)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def customer_params
+    params.require(:customer).permit(:full_name, :phone_number, :email_address, :notes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :customers
+  resources :customers do
+    collection do
+      get "missing_email"
+      get "alphabetized"
+    end
+  end
 
   root to: "customers#index"
 end
